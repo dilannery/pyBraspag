b11108f24fd2a614fdb4cf86b9ace594b3f80a42
Dilan Nery Lopes <dnerylopes@gmail.com>
2011-01-04 12:22:07 -0300

    Alguns metódos para a classe PagadorAuthorizeRequest

--- src/pagadorauthorize.py
+++ src/pagadorauthorize.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from lxml import etree
-import sys
 
 class PagadorAuthorizeRequest(object):
     def __init__(self, orderId):
@@ -11,7 +10,17 @@ class PagadorAuthorizeRequest(object):
         order_id.text = orderId
         self.request = request
         
-    def setCard(self, cardHolder, cardNumber, cardExpirationDate, 
+    def set_customerName(self, name):
+        custumer_name = etree.SubElement(self.request, 'CustomerName')
+        custumer_name.text = name
+        return self
+        
+    def set_valor(self, valor):
+        _valor = etree.SubElement(self.request, 'TransactionAmount')
+        _valor.text = valor
+        return self
+        
+    def set_card(self, cardHolder, cardNumber, cardExpirationDate, 
                 cardSecurityCode):
         card_holder = etree.SubElement(self.request, 'CardHolder')
         card_holder.text = cardHolder
@@ -21,7 +30,38 @@ class PagadorAuthorizeRequest(object):
         card_expirationdate.text = cardExpirationDate
         card_securitycode = etree.SubElement(self.request, 'CardSecurityCode')
         card_securitycode.text = cardSecurityCode
-    
+        return self
+        
+    def set_parcelas(self, numero):
+        parcelas = etree.SubElement(self.request, 'InstallmentCount')
+        parcelas.text = numero
+        return self
+        
+    def set_cpf(self, numero):
+        cpf = etree.SubElement(self.request, 'Cpf')
+        cpf.text = numero
+        return self
+        
+    def set_cnpj(self, numero):
+        cnpj = etree.SubElement(self.request, 'Cnpj')
+        cnpj.text = numero
+        return self
+        
+    def set_data(self, data):
+        _data = etree.SubElement(self.request, 'MerchantLocalDate')
+        _data.text = data
+        return self
+        
+    def set_buyerInfo(self, ip, email):
+        buyer_ip = etree.SubElement(self.request, 'BuyerIp')
+        buyer_ip.text = ip
+        buyer_email = etree.SubElement(self.request, 'BuyerE-mail')
+        return self
+        
+    def set_tipoDePagamento(self, numero):
+        tipo_de_pagamento = etree.SubElement(self,request, 'PaymentType')
+        tipo_de_pagamento.text = numero
+        return self
     
     def get_xml(self):
-        return etree.tostring(self.request)    
+        return etree.tostring(self.request)
